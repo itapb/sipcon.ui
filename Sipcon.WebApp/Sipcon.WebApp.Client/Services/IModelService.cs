79debe7bb72f0e5ab303ed8af74df0dff74e235d
yBiarreta <38255744+yBiarreta@users.noreply.github.com>
2025-05-09 13:39:42 -0400

    Sprint1 ybiarreta (#3)
    * Cambios clases Vehiculos  y Moculos
    
    * Cambios Vehiculos y Modelos
    
    * Homologacion Programs
    
    * Cambios Filtar por texto busqueda
    
    ---------
    
    Co-authored-by: itapb <wilmancamacho@apbgroup.net>

--- Sipcon.WebApp/Sipcon.WebApp.Client/Services/IModelService.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Services/IModelService.cs
@@ -4,7 +4,7 @@
     public interface IModelService
     {
      
-        public Task<ApiResponse<List<Model>>> GetModels(int IdUser, int RowFrom = 0, string Filter = " ");
+        public Task<ApiResponse<List<Model>>> GetModels(int IdUser, int RowFrom = 0, string Filter = "");
         public Task<ApiResponse<Model>> GetModel(int IdModel, int IdUser); 
         public Task<ApiResponse<List<ActionResult>>> CreateModel(Model Model, int IdUser);
         public Task<ApiResponse<List<ActionResult>>> UpdateModel(Model Model, int IdUser);
