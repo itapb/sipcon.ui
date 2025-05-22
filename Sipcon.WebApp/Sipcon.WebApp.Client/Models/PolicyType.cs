c518a09cb0310fc5088e652c526862a14017c8bd
ybiarreta <yonder.biarreta@gmail.com>
2025-05-22 15:33:09 -0400

    Cambios Tipo Poliza y standar Modal

--- Sipcon.WebApp/Sipcon.WebApp.Client/Models/PolicyType.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/PolicyType.cs
@@ -30,7 +30,9 @@ namespace Sipcon.WebApp.Client.Models
         [Required]
         public int? TopMonths { get; set; } = null;
 
+        [Required]
         public int? BrandId { get; set; } = null;
+
         public string BrandName { get; set; } = string.Empty;
 
         [Required]
