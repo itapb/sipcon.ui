9042d63791cdbced5e32d931f3f4dafc78981140
ybiarreta <yonder.biarreta@gmail.com>
2025-07-08 23:59:18 -0400

    Mejoras en la interfaz y lógica de contacto y vehículo
    Se ha hecho el título de la sección de proveedores dinámico en `ContactsCatalog.razor`.
    En `PolicyDialog.razor`, se han añadido condiciones para mostrar botones de exportación y se han reorganizado métodos para la búsqueda de vehículos y contactos.
    
    Se ha creado un nuevo componente en `VehicleCatalog.razor` para visualizar vehículos disponibles.
    
    En `ContactRepository.cs` y `VehicleRepository.cs`, se han actualizado métodos para incluir nuevos parámetros que permiten filtrar por tipo de contacto y concesionario, respectivamente.
    
    Se han ajustado las interfaces `IContactService.cs` y `IVehicleService.cs` para reflejar estos cambios.
    
    Además, se han mejorado los mensajes de error y se ha incrementado la legibilidad del código en varios archivos.

--- Sipcon.WebApp/Sipcon.WebApp.Client/Services/IVehicleService.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Services/IVehicleService.cs
@@ -5,9 +5,11 @@
     public interface IVehicleService
     {
 
-        public Task<ApiResponse<List<Vehicle>>> GetVehicles(int IdUser, int RowFrom = 0, string Filter = "");
+        public Task<ApiResponse<List<Vehicle>>> GetVehicles(int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null);
+        public Task<ApiResponse<List<Vehicle>>> GetVehiclesAvailables(int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null);
         public Task<ApiResponse<Vehicle>> GetVehicle(int IdVehicle, int IdUser);
-        public Task<ApiResponse<Vehicle>> GetVehicleBy(string Search, int IdUser, int IdDealer, SearchByEnum SearchBy);
+        public Task<ApiResponse<Vehicle>> GetVehicleBy(string Search, int IdUser, SearchByEnum SearchBy, int? IdDealer);
+        public Task<ApiResponse<Vehicle>> GetVehicleAvailable(string Search, int IdUser, int? IdDealer = null);
         public Task<ApiResponse<List<ActionResult>>> CreateVehicle(Vehicle Vehicle, int IdUser);
         public Task<ApiResponse<List<ActionResult>>> UpdateVehicle(Vehicle Vehicle, int IdUser);
         public Task<ApiResponse<List<ActionResult>>> ActionsVehicle(List<PostAction> PostActions, int IdUser);
