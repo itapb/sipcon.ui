1c92303432c65d64f86ed0d3a0edf5e77b43b0ac
Jorge Pedraza <jorgepedraza@apbgroup.net>
2025-09-17 18:28:09 -0400

    Update 1

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/AdjustmentDetails.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sipcon.WebApp.Client.Models
+{
+    public class AdjustmentDetails : Record
+    {
+        [Required]
+        public int? AdjustmentId { get; set; }
+
+        [Required]
+        public int? LocationId { get; set; }
+
+        [Required]
+        public int? PartId { get; set; }
+
+        [Required]
+        public int? Stock { get; set; }
+
+        [Required]
+        public int? ReasonId { get; set; }
+
+        [Required]
+        public string? AdjustmentType { get; set; }
+
+       
+        public string? Zone { get; set; }
+
+        
+        public string? Warehouse { get; set; }
+
+        
+        public string? Location { get; set; }
+
+        
+        public string? Inncercode { get; set; }
+
+       
+        public string? PartDescription { get; set; }
+
+       
+        public string? PartPrice { get; set; }
+
+        
+        public string? PartSize { get; set; }
+
+       
+        public string? ReasonDescription { get; set; }
+    }
+}
