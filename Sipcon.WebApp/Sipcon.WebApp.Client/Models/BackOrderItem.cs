d9d0b92fd808029002e4f86c947e1f357305c2d1
Wilman Camacho <wilmancamacho@apbgroup.net>
2025-09-29 11:28:09 -0400

    fix sale order-backorders

--- Sipcon.WebApp/Sipcon.WebApp.Client/Models/BackOrderItem.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/BackOrderItem.cs
@@ -2,8 +2,10 @@
 {
     public class BackOrderItem
     {
-        public int? BackOrderId { get; set; }
+        public int? Id { get; set; }
         public int? Quantity { get; set; }
         public DateTime? ArrivalDate { get; set; }
+
+        public bool? IsActive { get; set; } = true;
     }
 }
