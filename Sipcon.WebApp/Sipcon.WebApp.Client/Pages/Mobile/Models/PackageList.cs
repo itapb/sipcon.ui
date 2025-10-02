e15df8d84176ba93bb2cafe8175d85bee65498d1
Jorge Pedraza <jorgepedraza@apbgroup.net>
2025-10-02 11:55:53 -0400

    Update 10 Mobile Packing List

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Pages/Mobile/Models/PackageList.cs
@@ -0,0 +1,24 @@
+﻿namespace Sipcon.WebApp.Client.Pages.Mobile.Models
+{
+    public class PackageList
+    {
+       
+        public Int32? Id { get; set; }
+       
+        public string? Code { get; set; }
+       
+        public Int32? SupplierId { get; set; }
+       
+        public Int32? CustomerId { get; set; }
+       
+        public string? SupplierName { get; set; }
+       
+        public string? CustomerName { get; set; }
+      
+        public string? PartInnerCode { get; set; }
+       
+        public string? PartName { get; set; }
+       
+        public Int32? Quantity { get; set; }
+    }
+}
