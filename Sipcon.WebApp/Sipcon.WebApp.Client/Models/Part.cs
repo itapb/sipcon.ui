4235b549012e2c2296263cb67bec9139776b4e81
Wilman Camacho <wilmancamacho@apbgroup.net>
2025-10-07 16:30:54 -0400

    fix

--- Sipcon.WebApp/Sipcon.WebApp.Client/Models/Part.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/Part.cs
@@ -39,7 +39,7 @@ namespace Sipcon.WebApp.Client.Models
         [Required]
         public decimal? Cost { get; set; } = decimal.Zero;
 
-        [Required]
+       
         public decimal? Discount { get; set; } = decimal.Zero;
 
         [Required]
