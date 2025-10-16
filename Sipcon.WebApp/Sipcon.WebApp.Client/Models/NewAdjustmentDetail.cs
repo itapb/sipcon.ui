644e07ce9acd2bd179480d5f24c49544cba142d0
Jorgeapbgroup <jorgepedraza@apbgroup.net>
2025-10-16 12:05:54 -0400

    Sprint7 3 jorge (#68)
    * Update 1
    
    * Update 2

--- Sipcon.WebApp/Sipcon.WebApp.Client/Models/NewAdjustmentDetail.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/NewAdjustmentDetail.cs
@@ -4,6 +4,7 @@
     {
         public int?  AdjustmentId { get; set; } = 0;
         public int?  LocationId { get; set; } = 0;
+        public string? LocationName { get; set; }
         public int?  PartId { get; set; } = 0;
         public int?  Stock { get; set; } = 0;
         public int?  ReasonId { get; set; } = 0;
