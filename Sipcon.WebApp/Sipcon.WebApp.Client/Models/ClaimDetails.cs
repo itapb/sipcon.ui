8c538b2cf5ea18286fec68850e339918686e36f6
jorgeapbgroup <JorgePedraza@apbgroup.net>
2026-01-18 12:25:13 -0400

    Claim 4

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/ClaimDetails.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sipcon.WebApp.Client.Models
+{
+    public class ClaimDetails  : Record
+    {
+        [Required]
+        public int? ClaimId { get; set; }
+
+        [Required]
+        public int? PartId { get; set; }
+
+        [Required]
+        public int? Quantity { get; set; }
+
+        [Required]
+        public int? ReplacementPartId { get; set; }
+
+        [Required]
+        public int? ReasonId { get; set; }
+
+
+        public string? PartInnerCode { get; set; }
+
+
+        public string? PartName { get; set; }
+
+
+        public string? RemplacemetInnerCode { get; set; }
+
+
+        public string? ReplacemetName { get; set; }
+
+
+        public string? ReasonDescription { get; set; }
+
+
+        public string? StatusName { get; set; }
+
+
+        public string? Comment { get; set; }
+
+
+        public int? IApproved { get; set; }
+
+        public decimal? SubTotal { get; set; }
+    }
+}
---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/NewClaimOrderDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Sipcon.WebApp.Client.Models
+{
+    public class NewClaimOrderDetails : Record
+    {
+        public int? ClaimOrderId { get; set; } = 0;
+        public int? PartId { get; set; } = 0;
+        public int? Quantity { get; set; } = 0;
+        public int? ReasonId { get; set; } = 0;
+    
+    }
+}
