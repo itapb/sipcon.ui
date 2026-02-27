254a2ce3b11afb103e8794841fd1b9353fc919d6
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-02-27 13:39:03 -0400

    arreglo de fecha en reporte de falla y mantenimiento

--- Sipcon.WebApp/Sipcon.WebApp.Client/Models/Maintenance.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/Maintenance.cs
@@ -13,7 +13,8 @@ namespace Sipcon.WebApp.Client.Models
         public string ServiceTypeName { get; set; } = string.Empty;
         public int? ReportTypeId { get; set; } = null;
         public string ReportTypeName { get; set; } = string.Empty;
-        public DateTime? ServiceDate { get; set; } = null;
+
+        private DateTime? _serviceDate = DateTime.Today;
         public DateTime? DateCreated { get; set; } = null;
         public string DealerReport { get; set; } = string.Empty;
         public string SupplierReport { get; set; } = string.Empty;
@@ -25,6 +26,14 @@ namespace Sipcon.WebApp.Client.Models
         public string EstatusName { get; set; } = string.Empty;
         public string AuthorizedUserName { get; set; } = string.Empty;
         public string AuthorizedUserLastName { get; set; } = string.Empty;
+
+        public DateTime? ServiceDate
+        {
+            get => _serviceDate;
+            // El .Date elimina cualquier residuo de tiempo (horas, min, seg)
+            set => _serviceDate = value?.Date;
+        }
+
 
 
         /// <summary>
