bf5347318667232dbbd399c86235a891a374e68a
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-03-04 22:21:15 -0400

    arreglo de boton de edicion en columnas de reporte de falla

--- Sipcon.WebApp/Sipcon.WebApp.Client/Repository/FailReportRepository.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Repository/FailReportRepository.cs
@@ -51,6 +51,41 @@
 
         }
 
+
+        public async Task<ApiResponse<List<FailReport>>> GetServiceFailClosed(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null, string DateFrom = "", string DateTo = "", int? EstatusId = null)
+        {
+            ApiResponse<List<FailReport>>? result;
+
+            try
+            {
+                var url = $"api/Service/GetServiceFailClosed?supplierId={IdSupplier}&userId={IdUser}&rowFrom={RowFrom}";
+                url = (IdDealer.HasValue) ? $"{url}&dealerId={IdDealer}" : url;
+                url = string.IsNullOrEmpty(Filter) ? url : $"{url}&filter={Filter}";
+                url = string.IsNullOrEmpty(DateFrom) ? url : $"{url}&fromDate={DateFrom}";
+                url = string.IsNullOrEmpty(DateTo) ? url : $"{url}&upToDate={DateTo}";
+                url = (EstatusId.HasValue) ? $"{url}&estatusId={EstatusId}" : url;
+
+                result = await _http.GetFromJsonAsync<ApiResponse<List<FailReport>>>(url);
+
+                result = result is null ? new ApiResponse<List<FailReport>>()
+                {
+                    Processed = false,
+                    Message = "La respuesta del servidor no contiene datos.",
+                } : result;
+
+            }
+            catch (Exception ex)
+            {
+                result = new ApiResponse<List<FailReport>>()
+                {
+                    Processed = false,
+                    Message = string.Concat("Ocurrió un error inesperado: ", ex.Message)
+                };
+            }
+
+            return result;
+
+        }
         public async Task<ApiResponse<List<FailReportType>>> GetFailReportTypes(int IdUser)
         {
             ApiResponse<List<FailReportType>>? result;
