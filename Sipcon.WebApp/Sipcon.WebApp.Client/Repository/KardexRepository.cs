12f73ce5489ff75f873c904e6bfbf7e624d79ced
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-03-08 11:48:43 -0400

    creacion de vistas de kardex

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Repository/KardexRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace Sipcon.WebApp.Client.Repository
+{
+    using Sipcon.WebApp.Client.Services;
+    using Sipcon.WebApp.Client.Models;
+    using Sipcon.WebApp.Client.Enum;
+    using System.Net.Http.Json;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using Microsoft.Extensions.Options;
+
+
+    public class KardexRepository(HttpClient http) : IKardexService
+    {
+        private readonly HttpClient _http = http;
+
+        
+        public async Task<ApiResponse<List<Kardex>>> GetKardex(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null, string DateFrom = "", string DateTo = "")
+        {
+            ApiResponse<List<Kardex>>? result;
+
+            try
+            {
+                var url = $"api/GetKardex?supplierId={IdSupplier}&userId={IdUser}&rowFrom={RowFrom}";
+                url = (IdDealer.HasValue ) ? $"{url}&dealerId={IdDealer}" : url;
+                url = string.IsNullOrEmpty(Filter) ? url : $"{url}&filter={Filter}";
+                url = string.IsNullOrEmpty(DateFrom) ? url : $"{url}&fromDate={DateFrom}";
+                url = string.IsNullOrEmpty(DateTo) ? url : $"{url}&upToDate={DateTo}";
+
+                result = await _http.GetFromJsonAsync<ApiResponse<List<Kardex>>>(url);
+                
+
+                result = (result is null) ? new ApiResponse<List<Kardex>>()
+                {
+                    Processed = false,
+                    Message = "La respuesta del servidor no contiene datos."
+                } : result;
+
+            }
+            catch (Exception ex)
+            {
+                result = new ApiResponse<List<Kardex>>()
+                {
+                    Processed = false,
+                    Message = string.Concat("Ocurrió un error inesperado: ", ex.Message)
+                };
+            }
+
+            return result;
+
+        }
+
+      
+
+    }
+
+}
---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Services/IKardexService.cs
@@ -0,0 +1,10 @@
+﻿namespace Sipcon.WebApp.Client.Services
+{
+    using Sipcon.WebApp.Client.Enum;
+    using Sipcon.WebApp.Client.Models;
+    public interface IKardexService
+    {
+
+        public Task<ApiResponse<List<Kardex>>> GetKardex(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null, string DateFrom = "", string DateTo = "");
+    }
+}
