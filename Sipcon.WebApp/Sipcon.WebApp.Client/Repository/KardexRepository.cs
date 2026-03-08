b4f0cf1b2c8311afc69e3210473c69ee02a4ed9e
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-03-08 12:17:23 -0400

    exportacion de kardex

--- Sipcon.WebApp/Sipcon.WebApp.Client/Repository/KardexRepository.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Repository/KardexRepository.cs
@@ -14,14 +14,13 @@
         private readonly HttpClient _http = http;
 
         
-        public async Task<ApiResponse<List<Kardex>>> GetKardex(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null, string DateFrom = "", string DateTo = "")
+        public async Task<ApiResponse<List<Kardex>>> GetKardex(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "",  string DateFrom = "", string DateTo = "")
         {
             ApiResponse<List<Kardex>>? result;
 
             try
             {
                 var url = $"api/GetKardex?supplierId={IdSupplier}&userId={IdUser}&rowFrom={RowFrom}";
-                url = (IdDealer.HasValue ) ? $"{url}&dealerId={IdDealer}" : url;
                 url = string.IsNullOrEmpty(Filter) ? url : $"{url}&filter={Filter}";
                 url = string.IsNullOrEmpty(DateFrom) ? url : $"{url}&fromDate={DateFrom}";
                 url = string.IsNullOrEmpty(DateTo) ? url : $"{url}&upToDate={DateTo}";
@@ -49,7 +48,48 @@
 
         }
 
-      
+
+        public async Task<ApiResponse<List<byte>>> ExportKardex(int IdSupplier, int IdUser, string Filter = "", string DateFrom = "", string DateTo = "")
+        {
+            ApiResponse<List<byte>> result;
+            string fileUrl = string.Empty;
+
+            try
+            {
+                var url = $"api/ExportKardex?supplierId={IdSupplier}&userId={IdUser}&serviceTypeId={(int)ServiceTypeEnum.Maintenance}";
+                url = string.IsNullOrEmpty(Filter) ? url : $"{url}&filter={Filter}";
+                url = string.IsNullOrEmpty(DateFrom) ? url : $"{url}&fromDate={DateFrom}";
+                url = string.IsNullOrEmpty(DateTo) ? url : $"{url}&upToDate={DateTo}";
+
+                var response = await _http.GetAsync(url);
+
+                var fileContent = await response.Content.ReadAsByteArrayAsync();
+                result = (fileContent is null) ? new ApiResponse<List<byte>>()
+                {
+                    Processed = false,
+                    Message = "Error al Exportar Data.",
+                    Data = []
+                } : new ApiResponse<List<byte>>()
+                {
+                    Processed = true,
+                    Message = "",
+                    Data = fileContent.ToList()
+                };
+            }
+            catch (Exception ex)
+            {
+                result = new ApiResponse<List<byte>>()
+                {
+                    Processed = false,
+                    Message = string.Concat("Ocurrió un error inesperado: ", ex.Message),
+                    Data = []
+                };
+            }
+
+            return result;
+        }
+
+
 
     }
 
--- Sipcon.WebApp/Sipcon.WebApp.Client/Services/IKardexService.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Services/IKardexService.cs
@@ -5,6 +5,7 @@
     public interface IKardexService
     {
 
-        public Task<ApiResponse<List<Kardex>>> GetKardex(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "", int? IdDealer = null, string DateFrom = "", string DateTo = "");
+        public Task<ApiResponse<List<Kardex>>> GetKardex(int IdSupplier, int IdUser, int RowFrom = 0, string Filter = "", string DateFrom = "", string DateTo = "");
+        public Task<ApiResponse<List<byte>>> ExportKardex(int IdSupplier, int IdUser, string Filter = "", string DateFrom = "", string DateTo = "");
     }
 }
