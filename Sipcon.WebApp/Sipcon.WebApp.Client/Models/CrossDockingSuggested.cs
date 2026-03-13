c65e681afa9b92b4aff3b408425c48091e6582ad
lalo-7 <jimenezgerardo2001@gmail.com>
2026-03-13 08:41:31 -0400

    Cambios de crossdocking implementado

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/CrossDockingSuggested.cs
@@ -0,0 +1,9 @@
+﻿namespace Sipcon.WebApp.Client.Models
+{
+    public class CrossDockingSuggested
+    {
+        public string? PartCode { get; set; }
+        public string? PartDescription { get; set; }
+        public int RequestedQuantity { get; set; }
+    }
+}
