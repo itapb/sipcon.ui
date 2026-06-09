1772ce4bd14a3f595261fb35b751126f6a4cd3eb
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-06-09 16:32:33 -0400

    details

--- Sipcon.WebApp/Sipcon.WebApp.Client/Models/Payments.cs
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/Payments.cs
@@ -146,6 +146,8 @@ namespace Sipcon.WebApp.Client.Models
         public double? PaidAmount { get; set; } = null;
         public double? PaidAmountBs { get; set; } = null;
         public string TransactionStatusName { get; set; } = string.Empty;
+        public bool? IsSelected { get; set; } = null;
+
 
     }
 
