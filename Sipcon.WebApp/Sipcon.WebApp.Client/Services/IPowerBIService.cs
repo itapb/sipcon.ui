ea41ca9c015dff0247be1d193d4000a3e5c29370
Jose Diaz <josediaz@apbgroup.net>
2026-07-17 11:17:00 -0400

    Reportes de PowerBI dentro de SIPCON

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Services/IPowerBIService.cs
@@ -0,0 +1,14 @@
+namespace Sipcon.WebApp.Client.Services
+{
+    using Sipcon.WebApp.Client.Models;
+
+    public interface IPowerBIService
+    {
+        Task<ApiResponse<List<PowerBI_Reports>>> GetReportsPowerBI(int userId);
+
+        Task<ApiResponse<PowerBI_ReportPortal>> GetReportPortal(string reportId, string token);
+
+        Task<ApiResponse<PowerBI_Token>> GetTokenPowerBI();
+
+    }
+}
