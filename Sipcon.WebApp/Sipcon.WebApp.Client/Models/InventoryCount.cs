80584b77a2a91411579503d1e9a85edc6d41061b
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-07-26 20:00:56 -0400

    cambios conteo de inventario

---
+++ Sipcon.WebApp/Sipcon.WebApp.Client/Models/InventoryCount.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sipcon.WebApp.Client.Models
+{
+    public class InventoryCount : Record
+    {
+
+        [Required]
+        public String? Name { get; set; }
+
+        [Required]
+        public decimal? Amount { get; set; }
+
+    }
+}
